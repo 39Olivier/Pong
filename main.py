5cb620aa89d9465fb1f0f8928874a188f3f3fd2f
39Olivier <boite151@hotmail.fr>
2021-08-10 10:49:59 +0200

    Terminé !

--- main.py
+++ main.py
@@ -1,78 +1,42 @@
-# Débuté le 09/08/2021 Olivier Fontaine, voir les ToDo !
-#Git + Gitub !
+# https://youtu.be/C6jJg9Zan7w?  (CTRL+click pour ouvrir le lien !)
+# Github
+
+    # border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {} PlayerB: {}".format(score_a, score_b), align="center", font=("courrier", 24, "normal"))
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {} PlayerB: {}".format(score_a, score_b), align="center", font=("courrier", 24, "normal"))
+
+    # paddle colision
+    if (340 < ball.xcor() < 350) and (paddle_b.ycor() + 40 > ball.ycor() > paddle_b.ycor() - 50):
+        ball.setx(340)
+        ball.dx *= -1
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
+
+    if (-350 < ball.xcor() < -340) and (paddle_a.ycor() + 40 > ball.ycor() > paddle_a.ycor() - 50):
+        ball.setx(-340)
+        ball.dx *= -1
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
 
 
-import turtle
 
-wn = turtle.Screen()
-wn.title("Pong par Olivier")
-wn.bgcolor("black")
-wn.setup(width=800, height=600)
-wn.tracer()
 
-# Paddle A
-paddle_a = turtle.Turtle()
-paddle_a.speed(0)
-paddle_a.shape("square")
-paddle_a.color("white")
-paddle_a.shapesize(stretch_wid=5, stretch_len=1)
-paddle_a.penup()
-paddle_a.goto(-350, 0)
-
-# Paddle B
-paddle_b = turtle.Turtle()
-paddle_b.speed(0)
-paddle_b.shape("square")
-paddle_b.color("white")
-paddle_b.shapesize(stretch_wid=5, stretch_len=1)
-paddle_b.penup()
-paddle_b.goto(350, 0)
-
-# Balle
-ball = turtle.Turtle()
-ball.speed(0)
-ball.shape("square")
-ball.color("white")
-ball.penup()
-ball.goto(0, 0)
-ball.dx = 2
-ball.dy = 2
-# Function
-def paddle_a_up():
-    y = paddle_a.ycor()
-    y += 20
-    paddle_a.sety(y)
-
-def paddle_a_down():
-    y = paddle_a.ycor()
-    y -= 20
-    paddle_a.sety(y)
-
-def paddle_b_up():
-    y = paddle_b.ycor()
-    y += 20
-    paddle_b.sety(y)
-
-def paddle_b_down():
-    y = paddle_b.ycor()
-    y -= 20
-    paddle_b.sety(y)
-
-# Keyboard bindind
-wn.listen()
-wn.onkeypress(paddle_a_up, "w")
-wn.onkeypress(paddle_a_down, "s")
-wn.onkeypress(paddle_b_up, "Up")
-wn.onkeypress(paddle_b_down, "Down")
-
-# Main game loop
-while True:
-    wn.update()
-
-# move the ball
-ball.setx(ball.xcor() + ball.dx)
-ball.sety(ball.ycor() + ball.dy)  # todo 1 https://youtu.be/C6jJg9Zan7w?t=1028  (CTRL+click pour ouvrir le lien !
-
-ball.sety(ball.ycor() + ball.)
-
-#todo 2
